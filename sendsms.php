67afe56ae5aaca43be90f72dd0242443c295a716
Hannes "Brolaugh" Kindströmmer <hannes.kindstrommer@ip1.se>
2017-10-02 13:30:38 +0200

    SMSes are sent from and to recipients
    Instead of sending the message to the recipient it now wants a list of recipients. This list of recipients will be used to spoof the SMSes making it look like the SMSes came from one of the other recipeints.

--- sendsms.php
+++ sendsms.php
@@ -4,29 +4,62 @@ use IP1\RESTClient\SMS\OutGoingSMS;
 
 require_once('vendor/autoload.php');
 
-if (!preg_match('/(07|\+46)[0-9 ]{1,15}/', $_POST['recipeint'])) {
-    header("Location:/");
-}
-if (strlen($_POST['text']) > 160) {
-    header("Location:/");
-}
+
+// Initial setup
 $config = json_decode(file_get_contents('config.json'), true);
-
 $api = new Communicator($config['account'], $config['token']);
-$prefix = '07';
-$alternativePrefix = '+467';
-$cellphone = $_POST['recipeint'];
-if (substr($cellphone, 0, strlen($prefix)) == $prefix) {
-    $cellphone = substr($cellphone, strlen($prefix));
-    $cellphone = "467" . $cellphone;
-}
-if (substr($cellphone, 0, strlen($alternativePrefix)) == $alternativePrefix) {
-    $cellphone = substr($cellphone, strlen($alternativePrefix));
-    $cellphone = "467" . $cellphone;
-}
-$cellphone = str_replace(" ", "", $cellphone);
-$sms = new OutGoingSMS('iP1Demo', $_POST['text']);
-$sms->addNumber($cellphone);
-$api->add($sms);
 
-header("Location:/?sent=success");
+// If the text is too long we exit
+if (strlen($_POST['text']) > 160) {
+    //header("Location:/");
+    echo "length";
+}
+
+
+// Parse the text string into an array of numbers
+$phoneNumbers = explode("\n", trim($_POST['recipients']));
+$phoneNumbers = array_map(function($number){
+  return fixNumber($number);
+}, $phoneNumbers);
+shuffle($phoneNumbers);
+
+// Loop through the numbers sending
+// $i sends an SMS to $i+1
+for ($i=0; $i < count($phoneNumbers); $i++) {
+  $sender = $phoneNumbers[$i];
+  $recipient = $i != count($phoneNumbers) - 1 ? $phoneNumbers[$i + 1] : $phoneNumbers[0];
+
+  $sms = new OutGoingSMS($sender, $_POST['text']);
+  $sms->addNumber($recipient);
+  $api->add($sms);
+}
+
+//header("Location:/?sent=success");
+
+
+// Turns the numbers into the correct format
+function fixNumber(string $number): string{
+  $prefix = '07';
+  $alternativePrefix = '+467';
+
+  // Remove unnecessary symbols
+  $number = str_replace(['-','(',')'], '', $number);
+  $number = trim($number);
+
+  // If a number is invalid
+  if (!preg_match('/(07|\+46|46)[0-9]{1,15}/', $number)) {
+      //header("Location:/");
+      var_dump($number);
+  }
+
+  if (substr($number, 0, strlen($prefix)) == $prefix) {
+      $number = substr($number, strlen($prefix));
+      $number = "467" . $number;
+  }
+  if (substr($number, 0, strlen($alternativePrefix)) == $alternativePrefix) {
+      $number = substr($number, strlen($alternativePrefix));
+      $number = "467" . $number;
+      $number = str_replace(" ", "", $number);
+  }
+  return $number;
+}
