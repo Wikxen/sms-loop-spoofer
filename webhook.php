bab8e4b1692724e0c9d1ae6662e1b17e455d8108
Erik Wikström <wikxen@gmail.com>
2017-12-12 15:29:58 +0100

    Add sending webhook

--- webhook.php
+++ webhook.php
@@ -23,7 +23,23 @@ if (strtolower(substr($msg, 0, 5)) == "santa") { // signup
   $sms->addNumber($sender);
   $api->add($sms);
 } else if (strtolower(substr($msg, 0, 2)) == "go") { // send
+  // Parse the file string into an array of people
+  $file = file_get_contents("santa.txt");
+  $people = explode(PHP_EOL, $file);
+  array_pop($people);
+  $people = array_unique($people);
+  shuffle($people);
 
+  // Loop through the numbers sending
+  // $i sends an SMS to $i+1
+  for ($i=0; $i < count($people); $i++) {
+    $sender = explode(";", $people[$i]);
+    $recipient = explode(";", $i != count($people) - 1 ? $people[$i + 1] : $people[0]);
+
+    $sms = new OutGoingSMS($sender[0], "Hej " . $recipient[1] . "!\nDu fick mig. Köp något bra nu för ungefär 100 kr.\n// " . $sender[1]);
+    $sms->addNumber($recipient[0]);
+    $api->add($sms);
+  }
 }
 
 echo "OK";
