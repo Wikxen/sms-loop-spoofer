b35c909de5548f9dcc169a39346cb48db13b66f4
Erik Wikström <wikxen@gmail.com>
2017-12-12 14:13:19 +0100

    Add basic webhook endpoint

---
+++ webhook.php
@@ -0,0 +1,19 @@
+<?php
+use IP1\RESTClient\Core\Communicator;
+use IP1\RESTClient\SMS\OutGoingSMS;
+
+require_once('vendor/autoload.php');
+
+// Initial setup
+$config = json_decode(file_get_contents('config.json'), true);
+$api = new Communicator($config['account'], $config['token']);
+
+// handle parameters
+$sender = $_GET["sender"];
+$msg = $_GET["text"];
+
+if (substr($msg, 0, 5) == "santa") {
+
+} else if (substr($msg, 0, 2) == "go") {
+
+}
